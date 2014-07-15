e5ff9c0859e5059e5a77f228920a4497d4e25618
Imogen <imogen.wentworth@gmail.com>
2014-07-15 22:42:32 +0100

    Add basic searching and adding functionality

--- client/popcorn.js
+++ client/popcorn.js
@@ -1,63 +1,125 @@
 if (Meteor.isClient) {
 
+  Movies = new Meteor.Collection('movies');
+  Meteor.subscribe('movies');
+
   String.prototype.isEmpty = function() {
     return this.length === 0 || !this.trim();
   };
 
-  var keyAction = function(key) {
-    if (key === 8 || (key >= 48 && key <= 90))
-      return 'search';
-    else if (key === 40)
-      return 'downArrow';
+  var pressed = function(key) {
+    if (key >= 48 && key <= 90)
+      return 'alphNum';
     else if (key === 38)
-      return 'upArrow';
+      return 'up';
+    else if (key === 40)
+      return 'down';
+    else if (key === 13)
+      return 'enter';
+    else if (key === 8)
+      return 'delete';
   };
 
-  Template.search.helpers({
-    results: function() {
-      return Session.get("searchResults");
-    }
+  var searchApi = function(title) {
+    $.ajax({
+      url: 'http://www.omdbapi.com/?s=' + title,
+      dataType: 'json',
+      success: function(data, textStatus, jqXHR) {
+        if ('undefined' !== typeof data.Search)
+          Session.set('searchResults', data.Search);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error(errorThrown.message);
+      }
+    });
+  };
 
-  });
+  var fetchDetails = function(imdbId) {
+    // TODO if in DB, retrieve from there
+    $.ajax({
+      url: 'http://www.omdbapi.com/?i=' + imdbId,
+      dataType: 'json',
+      success: function(data, textStatus, jqXHR) {
+        // show modal overlay with movie data
+        Session.set('movieInfo', data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error(errorThrown.message);
+      }
+    });
+  };
+
+  Template.search.results = function() {
+    return Session.get('searchResults');
+  };
 
   Template.search.events({
     'keyup #search-form': function(event, template) {
-      if (keyAction(event.which) === 'search') {
+      var key = event.which;
+      if (pressed(key) === 'alphNum' || pressed(key) === 'delete') {
         var searchTerm = template.find('#search-form').value;
         if (searchTerm.isEmpty()) {
-          Session.set("searchResults", []);
+          Session.set('searchResults', []);
         } else {
-          // call to API
-          $.ajax({
-            url: 'http://www.omdbapi.com/?s=' + searchTerm,
-            dataType: 'json',
-            success: function(data, textStatus, jqXHR) {
-              if ('undefined' !== typeof data.Search)
-                Session.set('searchResults', data.Search);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-              console.error(errorThrown.message);
-            }
-          });
+          searchApi(searchTerm);
         }
       }
     },
-    'keypress #search-form': function(event, template) {
-      if (keyAction(event.which) === 'downArrow') {
-        console.log(this.firstNode);
-        console.log(this);
-        // if li.nextchild
-        // focus on next li down the list
-      } else if (keyAction(event.which) === 'upArrow') {
-        // if li.prevchild
-        // focus on next li up the list
+    'keydown #search-form': function(event, template) {
+      var key = event.which;
+      if (pressed(key) === 'enter') {
+        if (!_.isEqual(Session.get('searchResults'), [])) {
+          // TODO bring up info of first result
+          event.preventDefault();
+          fetchDetails(template.lastNode.firstElementChild.dataset.imdbid);
+          Session.set('searchResults', []);
+        }
+      }
+
+      if (pressed(key) === 'down') {
+        if (!_.isEqual(Session.get('searchResults'), []))
+          template.lastNode.firstElementChild.focus();
+      }
+    },
+    'keydown #results': function(event, template) {
+      var key = event.which;
+      if (pressed(key) === 'enter') {
+        event.preventDefault();
+        fetchDetails(event.target.dataset.imdbid);
+        Session.set('searchResults', []);
+        $('#search-form').val('');
+      }
+
+      if (pressed(key) === 'down') {
+        if (document.activeElement.nextElementSibling)
+          document.activeElement.nextElementSibling.focus();
+      }
+
+      if (pressed(key) === 'up') {
+        if (document.activeElement.previousElementSibling)
+          document.activeElement.previousElementSibling.focus();
+        else
+          $('#search-form').focus();
       }
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.modal.info = function() {
+    return Session.get('movieInfo');
+  };
+
+  Template.modal.events({
+    'click #add-movie': function(event, template) {
+      // add to DB
+      Movies.insert(Session.get('movieInfo'), function(err, id) {
+        if (err) console.log(err);
+        Session.set('movieInfo', []);
+        // close modal
+      });
+    }
   });
+
+  Template.movies.movies = function() {
+    return Movies.find({}).fetch();
+  };
 }
---
+++ server/publish.js
@@ -0,0 +1,16 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    Movies = new Meteor.Collection('movies');
+
+    // Movies.publish('movies', function() {
+    //   return Movies.find({});
+    // });
+
+    Movies.allow({
+      insert: function(userId, doc) {
+        return true;
+      }
+    });
+  });
+}
